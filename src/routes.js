0ee7dda7f40018573fb4cfce0fb6c3ea86548c8e
guistetter <guilherme_stetter@hotmail.com>
2020-10-08 16:31:30 -0300

    fix route address

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/" exact component={Cart} />
+      <Route path="/cart" exact component={Cart} />
     </Switch>
   );
 }
