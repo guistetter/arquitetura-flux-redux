e6c0bb1b6a6c444fd87a6d7b19dc6b86d670ba42
guistetter <guilherme_stetter@hotmail.com>
2020-10-09 13:27:43 -0300

    fix configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,20 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
-  parser: "babel-eslint",
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
-    "impor/prefer-default-export": "off",
+    'react/state-in-constructor': 'off',
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'impor/prefer-default-export': 'off',
   },
 };
