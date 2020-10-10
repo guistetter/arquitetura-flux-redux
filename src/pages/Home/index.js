1d2f34c1cf49ce1c34940630801e6f5b47a3c134
guistetter <guilherme_stetter@hotmail.com>
2020-10-10 17:17:38 -0300

    refactor actions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import api from '../../services/api';
+import * as CartActions from '../../store/modules/cart/actions';
+
 import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
 
@@ -20,11 +24,8 @@ class Home extends Component {
   }
 
   handleAddProduct = (product) => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    });
+    const { addToCart } = this.props;
+    addToCart(product);
   };
 
   render() {
@@ -51,4 +52,8 @@ class Home extends Component {
     );
   }
 }
-export default connect()(Home);
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(Home);
