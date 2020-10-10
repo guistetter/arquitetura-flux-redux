0b0653d35d54313499b4bddf8cee5fe5758b032f
guistetter <guilherme_stetter@hotmail.com>
2020-10-10 18:40:58 -0300

    calculate total

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -11,7 +11,7 @@ import { Container, ProductTable, Total } from './styles';
 import { formatPrice } from '../../util/format';
 import * as CartActions from '../../store/modules/cart/actions';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
   function increment(product) {
     updateAmount(product.id, product.amount + 1);
   }
@@ -75,7 +75,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
         <button type="button">Finalizar Pedido</button>
         <Total>
           <span>Total</span>
-          <strong>R$1920,30</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
@@ -87,6 +87,11 @@ const mapStateToProps = (state) => ({
     ...product,
     subtotal: formatPrice(product.price * product.amount),
   })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
 });
 
 const mapDispatchToProps = (dispatch) =>
