63561da60013215e51a172e3f96ea632f4f8822e
guistetter <guilherme_stetter@hotmail.com>
2020-10-12 15:53:21 -0300

    add rules react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,13 +12,15 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'react/state-in-constructor': 'off',
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'no-console':["error", {allow:["tron"]}],
-    'no-param-reassign': 'off'
+    'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
