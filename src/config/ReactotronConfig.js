26b40c1639c6ea382dd6ffb649769a13a0bc3313
guistetter <guilherme_stetter@hotmail.com>
2020-10-12 11:52:44 -0300

    add host address

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,7 +2,9 @@ import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().use(reactotronRedux()).connect();
+  const tron = Reactotron.configure({ host: '192.168.1.103' })
+    .use(reactotronRedux())
+    .connect();
   tron.clear();
   console.tron = tron;
 }
